abbac65da7f00308f43d37fb4c8e8ab100a18b61
tem <tim.munkhtur@gmail.com>
2024-12-02 16:20:16 -0500

    cleaned up MovieController, removed unused import

--- movies/src/main/java/dev/temmunk/movies/controller/MovieController.java
+++ movies/src/main/java/dev/temmunk/movies/controller/MovieController.java
@@ -2,7 +2,6 @@ package dev.temmunk.movies.controller;
 
 import dev.temmunk.movies.entity.Movie;
 import dev.temmunk.movies.service.MovieService;
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,16 +10,15 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin(origins="*")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("api/v1/movies")
 public class MovieController {
 
-
     private MovieService movieService;
 
     @Autowired
-    public MovieController(MovieService movieService){
+    public MovieController(MovieService movieService) {
         this.movieService = movieService;
     }
 
@@ -39,6 +37,5 @@ public class MovieController {
             return new ResponseEntity<>("Movie not found", HttpStatus.NOT_FOUND);
         }
     }
-
 
 }
